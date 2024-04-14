55336ac9c0260b740e8c10e3eff087dfa1c7319a
Bruno <albuquerquebruno2020@gmail.com>
2024-04-14 11:26:25 -0300

    Removing files

--- low-level/exe2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Bem vindo ao meu programa!");
-    printf("\nO produto de 8 por 90 é ", 8*90);
-    printf("\nO quociente de 90 por 30 é ", 90/30);
-    printf("\nO quadrado de 9 é ", 9^2);
-}   
